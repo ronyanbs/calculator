ad3e2d22cc708bba8eb0d0fb4d96a2bfc823efb0
ronyanbs <veronikamailll@gmail.com>
2024-03-07 11:14:46 +0300

    feat: добавили класс калькулятор

--- src/main/java/ru/krista/example/App.java
+++ src/main/java/ru/krista/example/App.java
@@ -12,48 +12,11 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        int num1;
-        int num2;
-        int r;
-        String op;
-
-        Scanner in = new Scanner(System.in);
-        System.out.printf("Введите выражение: "); //добавление комментария
-        String str = in.nextLine();
-        in.close();
-
-        String[] parts = str.split(" ");
-        try {
-            num1 = Integer.parseInt(parts[0]);
-            num2 = Integer.parseInt(parts[2]);
-         } catch (NumberFormatException e) {
-            System.out.println("Не удалось получить число");
-            return;
-         }
-       
-        op = parts[1];
-        switch (op) {
-            case "+":
-                r = num1 + num2;
-                break;
-            case "-":
-                r = num1 - num2;
-                break;
-            case "*":
-                r = num1 * num2;
-                break;
-            case "/":
-                if (num2 == 0) {
-                    System.out.printf("На ноль делить нельзя");
-                }
-                r = num1 / num2;
-                break;
-            default:
-                System.out.printf("Ошибка");
-                return;
-        }
-        System.out.print("\nРезультат:\n");
-        System.out.println(num1 + " " + op + " " + num2 + " = " + r);
+          
+        Calculator calculator = new Calculator();
+        calculator.Numbers();
+        calculator.Switch();
+        calculator.Output ();
     }
 
 }
--- src/main/java/ru/krista/example/Calculator.java
+++ src/main/java/ru/krista/example/Calculator.java
@@ -1,5 +1,72 @@
 package ru.krista.example;
 
+import java.util.Scanner;
+
 public class Calculator {
-    
+    public int num1;
+    public int num2;
+    public int result;
+    public String op;
+
+
+public void Numbers (){
+Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите выражение: ");
+    String input = scanner.nextLine();
+    scanner.close();
+    String[] parts = input.split(" ");
+    try {
+            num1 = Integer.parseInt(parts[0]);
+            num2 = Integer.parseInt(parts[2]);
+            op = parts [1];
+        } catch (NumberFormatException e) {
+        System.out.println("Не удалось получить число");
+        return;
+        }
+}
+
+public void Switch (){
+    switch (op) {
+        case "+":
+            add();
+            break;
+        case "-":
+            subtract ();
+            break;
+        case "*":
+            multiply();
+            break;
+        case "/":
+           div();
+            break;
+        default:
+            System.out.printf("Ошибка");
+            return;
+}
+}
+
+public void add (){
+    result = num1 + num2;
+
+}
+
+public void subtract (){
+    result = num1 - num2;
+}
+
+public void multiply (){
+    result = num1 * num2;
+}
+
+public void div (){
+    if (num2 == 0) {
+        System.out.printf("На ноль делить нельзя");
+        System.exit(1);
+    }
+    result = num1 / num2;
+}
+
+public void Output (){
+    System.out.print("\nРезультат:\n" + result );
+}
 }
